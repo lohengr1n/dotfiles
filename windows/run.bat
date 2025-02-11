bbd1814f31862bf74f8a08e817bda262e41d4f60
brandon <bantillon@pm.me>
2025-02-11 15:50:26 -0800

    Update run.bat

--- windows/run.bat
+++ windows/run.bat
@@ -1,2 +1,4 @@
 Rem simple script for just running winget and installing everythin on a fresh system.
+Rem the below powershell command sets the monitor sleep time to 30 seconds when locked
+Rem powercfg /SETDCVALUEINDEX SCHEME_CURRENT SUB_VIDEO VIDEOCONLOCK 30
 choco install packages.config -y
